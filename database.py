dd1f0e268da916436de1a2d49c0239e3caad57db
khandyman <timothy.s.wise@gmail.com>
2024-11-20 11:26:14 -0500

    Changed default user password in code due to public nature of github repo

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-params = "mysql+pymysql://tsw581:m1kayr1s@127.0.0.1/access_wv_online?charset=utf8mb4"
+params = "mysql+pymysql://tsw581:test_pass123@127.0.0.1/access_wv_online?charset=utf8mb4"
 engine = create_engine(params)
 
 with engine.connect() as conn:
