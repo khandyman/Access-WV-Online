49adbfc54d85a53501a30100fa4cf6e4c2e99c7a
khandyman <timothy.s.wise@gmail.com>
2024-11-22 10:36:00 -0500

    Mapped correct dynamic routing from log_in to device_connection and back

--- app.py
+++ app.py
@@ -3,49 +3,6 @@ from database import retrieve_records
 
 app = Flask(__name__)
 
-wire_centers_list = [
-    {
-        'id:': 0,
-        'name': 'Beckley'
-    },
-    {
-        'id:': 1,
-        'name': 'Charleston'
-    },
-    {
-        'id:': 2,
-        'name': 'Clarksburg'
-    },
-    {
-        'id:': 3,
-        'name': 'Logan'
-    },
-    {
-        'id:': 4,
-        'name': 'Martinsburg'
-    },
-    {
-        'id:': 5,
-        'name': 'Morgantown'
-    },
-    {
-        'id:': 6,
-        'name': 'Parkersburg'
-    },
-    {
-        'id:': 7,
-        'name': 'Weirton'
-    },
-    {
-        'id:': 8,
-        'name': 'Wheeling'
-    },
-    {
-        'id:': 9,
-        'name': 'Williamson'
-    },
-]
-
 
 @app.route('/')
 def log_in():
@@ -57,30 +14,39 @@ def network_elements():
     query = "SELECT * FROM network_elements"
     network_elements_list = retrieve_records(query)
 
-    return render_template('network_elements.html',
-                           network_elements=network_elements_list)
+    return render_template('network_elements.html', network_elements=network_elements_list)
 
 
-@app.route('/wire_centers/<device_type>')
-def wire_centers(device_type):
+@app.route('/wire_centers/<network_element>')
+def wire_centers(network_element):
     query = (f"SELECT DISTINCT wire_centers.name FROM wire_centers "
-             "INNER JOIN host_names ON wire_centers.abbr = host_names.location " 
+             "INNER JOIN host_names ON wire_centers.abbr = host_names.location "
              "INNER JOIN network_elements ON network_elements.name = host_names.type "
-             f"WHERE host_names.type = '{device_type}' ORDER BY wire_centers.name")
+             f"WHERE host_names.type = '{network_element}' ORDER BY wire_centers.name")
     wire_center_list = retrieve_records(query)
 
     return render_template('wire_centers.html',
-                           wire_centers=wire_center_list)
+                           network_element=network_element, wire_centers=wire_center_list)
 
 
-@app.route('/host_names')
-def host_names():
-    return render_template('host_names.html')
+@app.route('/host_names/<network_element>/<wire_center>')
+def host_names(network_element, wire_center):
+    query = (f"SELECT host_names.clli FROM host_names " 
+             "INNER JOIN wire_centers ON wire_centers.abbr = host_names.location " 
+             f"WHERE host_names.type = '{network_element}' AND wire_centers.name = '{wire_center}'"
+             "ORDER BY host_names.clli")
+    host_names_list = retrieve_records(query)
+
+    return render_template('host_names.html',
+                           network_element=network_element, wire_center=wire_center, host_names=host_names_list)
 
 
-@app.route('/device_connection')
-def device_connection():
-    return render_template('device_connection.html')
+@app.route('/device_connection/<network_element>/<wire_center>/<clli>')
+def device_connection(network_element, wire_center, clli):
+
+
+    return render_template('device_connection.html',
+                           network_element=network_element, wire_center=wire_center, clli=clli)
 
 
 if __name__ == '__main__':
